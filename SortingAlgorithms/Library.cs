fd767b6a04328cbcb3312056247e8f878bacc3bc
canUCMeSharp <tom.lochner07@outlook.com>
2022-06-27 00:18:11 +0200

    My first real commit!

--- SortingAlgorithms/Library.cs
+++ SortingAlgorithms/Library.cs
@@ -2,6 +2,73 @@
 {
     public class Library
     {
-
+        ///Welcome to the Sorting Algorithms Library!
+        ///This is a library, that's main purpose it is to get me some coding, computer science
+        ///and C# practise. This means that the code her mostly is pretty shit, but I haven't
+        ///learned to do it better yet. For the purpose of simplicity, I´monly gonna handle
+        ///integers.
+        ///HAVE FUN!
+    }
+    public partial class SortingAlgorithm
+    {
+        public bool isSorted(int[] array)
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                if(array[i] > array[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public void switchPlaces(int[] array, int posA, int posB)
+        {
+            int temp = array[posA];
+            array[posA] = array[posB];
+            array[posB] = temp;
+        }
+    }
+    public class BubbleSort : SortingAlgorithm
+    {
+        public void sort(int[] array)
+        {
+            while (!isSorted(array))
+            {
+                for(int i = 0; i < array.Length - 1; i++)
+                {
+                    if(array[i] > array[(i + 1)])
+                    {
+                        switchPlaces(array, i, i + 1);
+                    }
+                }
+            }
+        }
+    }
+    public class GnomeSort : SortingAlgorithm
+    {
+        public void sort(int[] array)
+        {
+            int arrayLength = array.Length;
+            for(int i =0; i < arrayLength - 1;)
+            {
+                if(array[i] > array[i + 1])
+                {
+                    switchPlaces(array, i, i + 1);
+                    if(i > 0)
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
     }
 }
---
+++ TestPlatform/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using SortingAlgorithms;
+
+namespace SortingAlgorithms
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.test(1000);
+        }
+    }
+    public class Test
+    {
+        BubbleSort bubbleSort = new BubbleSort();
+        GnomeSort gnomeSort = new GnomeSort();
+        Random random = new Random();
+
+        public void test(int length)
+        {
+            int[] a = new int[length];
+            bool allTestsComplete = true;
+            bool isSorted;
+
+            shuffle(a);
+            Console.WriteLine("The unsorted array:");
+            printArray(a);
+            bubbleSort.sort(a);
+            Console.WriteLine("The sorted array:");
+            printArray(a);
+            isSorted = this.isSorted(a);
+            Console.WriteLine(isSorted);
+            allTestsComplete = allTestsComplete && isSorted;
+
+            shuffle(a);
+            Console.WriteLine("The unsorted array:");
+            printArray(a);
+            gnomeSort.sort(a);
+            Console.WriteLine("The sorted array:");
+            printArray(a);
+            isSorted = this.isSorted(a);
+            Console.WriteLine(isSorted);
+            allTestsComplete = allTestsComplete && isSorted;
+
+            if (allTestsComplete)
+            {
+                Console.WriteLine("Everything works!");
+            }
+            else
+            {
+                Console.WriteLine("Fuck, something went wrong!");
+            }
+        }
+        private void shuffle(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next();
+            }
+        }
+        private void printArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        private bool isSorted(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
